599d1b5ea3b9d7135c0f6bc103eda595f81e1d2e
LeishaS1130 <83144209+LeishaS1130@users.noreply.github.com>
2022-05-08 16:18:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function one(){
+    window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states. It is the longest structure humans have ever built. It is about 21,196 kilometers long, 9.1 metres (30 feet) wide and 15 metres high. It is made over the course of hundreds of years, the wall was built by over 6 different Chinese dynasties, and is over 2,300 years old.")
+}
+
+function two(){
+    window.alert("Chichén Itzá, ruined ancient Maya city occupying an area of 4 square miles (10 square km) in south-central Yucatán state, Mexico. It is thought to have been a religious, military, political, and commercial centre that at its peak would have been home to 35,000 people.")
+}
+
+function three(){
+    window.alert("Al-Khazneh is one of the most elaborate temples in Petra, a city of the Nabatean Kingdom inhabited by the Arabs in ancient times. As with most of the other buildings in this ancient town, including the Monastery, this structure was carved out of a sandstone rock face. The structure is believed to have been the mausoleum of the Nabatean King Aretas IV in the 1st century AD. It is one of the most popular tourist attractions in both Jordan and the region. It became known as 'Al-Khazneh', or The Treasury, in the early 19th century by the area's Bedouins as they had believed it contained treasures.")
+}
+
+function four(){
+    window.alert("Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments and panoramic views. Its exact former use remains a mystery.")
+}
+
+function five(){
+    window.alert("Christ the Redeemer is a 30 metre high statue, mounted upon an 8 metre pedestal, and of course the 700 metre high mountain of Corcovado. It was constructed between 1922 and 1931 from soapstone and reinforced concrete. Today it is a Brazilian cultural icon and global symbol of Christianity.")
+}
+
+function six(){
+    window.alert("The Colosseum is an amphitheatre built in Rome under the Flavian emperors of the Roman Empire. It is an elliptical structure made of stone, concrete, and tuff, and it stands four stories tall at its highest point. It measures 620 by 513 feet and could hold as many as 50,000 spectators. The Colosseum was famously used for gladiatorial combat.")
+}
+
+function seven(){
+    window.alert("The Taj Mahal is a mausoleum complex in Agra, western Uttar Pradesh state, northern India. It is considered the finest example of Mughal architecture (a blend of Indian, Persian, and Islamic styles). The Taj Mahal is also one of the world’s most iconic monuments, visited by millions of tourists each year.")
+}
